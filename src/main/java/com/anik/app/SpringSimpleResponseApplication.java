d53e7c4e3c753db081699aa3050c8007a0f49a20
Kafi <kafi@itconquest.com>
2024-02-08 16:15:12 +0600

    seeder added

--- src/main/java/com/anik/app/SpringSimpleResponseApplication.java
+++ src/main/java/com/anik/app/SpringSimpleResponseApplication.java
@@ -1,10 +1,8 @@
 package com.anik.app;
 
-import com.anik.app.dto.author.AuthorDto;
 import com.anik.app.dto.author.CreateAuthorDto;
 import com.anik.app.entity.author.Author;
 import com.anik.app.mapper.AuthorMapper;
-import com.anik.app.mapper.CycleAvoidingMappingContext;
 import com.anik.app.repository.AuthorRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
@@ -31,20 +28,22 @@ public class SpringSimpleResponseApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        /*try (InputStream inputStream = new ClassPathResource("/jsonData/seed.json").getInputStream()) {
+    public void run(String... args) {
+        try (InputStream inputStream = new ClassPathResource("/jsonData/seed.json").getInputStream()) {
             ObjectMapper objectMapper = new ObjectMapper();
-            CreateAuthorDto authorDto = objectMapper.readValue(inputStream, CreateAuthorDto.class);
-            Author author = this.authorMapper.toEntity(authorDto, new CycleAvoidingMappingContext());
-            UUID uuid = UUID.fromString("6405cb54-09a9-480a-93b0-81c13c010c1b");
-            author.setId(uuid);
-            author.getBooks().forEach(book -> {
-                book.setId(UUID.randomUUID());
-                book.setAuthor(author);
-            });
-            this.authorRepository.save(author);
+            CreateAuthorDto[] authorDtos = objectMapper.readValue(inputStream, CreateAuthorDto[].class);
+            for(var authorDto : authorDtos) {
+                Author author = this.authorMapper.toEntity(authorDto);
+                UUID uuid = UUID.randomUUID();
+                author.setId(uuid);
+                author.getBooks().forEach(book -> {
+                    book.setId(UUID.randomUUID());
+                    book.setAuthor(author);
+                });
+                this.authorRepository.save(author);
+            }
         } catch (IOException e) {
             log.info(e.toString());
-        }*/
+        }
     }
 }
